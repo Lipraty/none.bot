19408171e3571e914c2d4a74c4ebfbdbc1566ae9
Lipraty <sis1943@outlook.com>
2023-12-23 00:06:52 +0800

    Delete terms of service HTML file and comment out build configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  // build: {
+  //   lib: {
+  //     entry: './termsofservice.html',
+  //     name: 'Termsofservice',
+  //   }
+  // }
 })
